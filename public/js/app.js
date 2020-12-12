efbd57923773878bb2eb6cfbd37d52119467d96d
udgar <43161200+udgar@users.noreply.github.com>
2020-12-12 16:54:30 +0545

    add template

--- public/js/app.js
+++ public/js/app.js
@@ -2749,6 +2749,26 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   mounted: function mounted() {
     console.log('Component mounted.');
